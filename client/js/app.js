3fe1da34870147df71326dcd9735129126858694
Neil Jones <neil.j.neil@gmail.com>
2017-04-15 12:42:43 -0500

    Added the autocomplete feature and multi search

--- client/js/app.js
+++ client/js/app.js
@@ -1,7 +1,5 @@
-// App
-var app = angular.module('app', []);
+var app = angular.module('app', ['autocomplete']);
 
-// Service to fetch some data..
 app.factory('dataServ', ['$http', function ($http) {
     return {
         get: function () {
@@ -10,7 +8,6 @@ app.factory('dataServ', ['$http', function ($http) {
     }
 }]);
 
-// App controller
 app.controller('appController', ['$scope', 'dataServ', '$http', '$window', function ($scope, Data, $http, $window) {
 
     var mymap = L.map('mapid').setView([32.7311, -97.1141], 15);
@@ -24,6 +21,93 @@ app.controller('appController', ['$scope', 'dataServ', '$http', '$window', funct
 
         $scope.result = [];
         $scope.detailedResult = [];
+
+        $scope.radius = "";
+        $scope.from = "";
+
+        $scope.names = [
+"Engineering Research Building",
+"Nedderman Hall",
+"Geoscience",
+"Engineering Lab Building",
+"Woolf Hall",
+"Science Hall",
+"Preston Hall",
+"Ransom Hall",
+"Carlisle Hall",
+"College Hall",
+"Business Building",
+"Trimble Hall",
+"Hammond Hall",
+"Life Science Building",
+"University Hall",
+"Davis Hall",
+"Texas Hall",
+"Pickard Hall",
+"Aerodynamics Research Building",
+"Continuing education / workforce Development",
+"DED Technical Training Center",
+"University Center",
+"Maveric Activities Center",
+"Physical Education",
+"Campus Center",
+"Nanotech Building",
+"Cappa Building",
+"Fine Arts Building",
+"Smart Hospital",
+"Planetarium",
+"Swift Center",
+"Veterans Assistance Center",
+"Child Development Center",
+"Environment Health and Safety",
+"University Police",
+"Office and Classroom Building",
+"Studio Arts Center",
+"Amphibian and reptile diversity research center",
+"Civil engineering lab building",
+"Wetsel Building",
+"Arbor Oaks",
+"Arlington Hall",
+"Brazos House",
+"Centennial Court",
+"Center Point",
+"Cooper Chase",
+"Maple Square",
+"Garden Club",
+"Kalpana Chawla Hall",
+"Lipscomb Hall",
+"Timberbrook",
+"Meadow Run",
+"The Heights on Pecan",
+"The Lofts at College park",
+"Trinity House",
+"University Village",
+"Vandergiff Hall",
+"Campus Edge",
+"The Arlie",
+"Heatherway",
+"Midtown Arlington Apartments",
+"Caribe Apartments",
+"Mary Kay Apartments",
+"Zen Apartments",
+"Four oak Apartments",
+"South Campus Apartments",
+"University Terrace Apartments",
+"Mesa Apartments",
+"Maverick Place Apartments",
+"Linda Vista Apartments",
+"Sam Maverick Apartments",
+"Pepper Mill Apartments",
+"Pinewoods Apartments",
+"Vintage Pads",
+"Summit Plaza Apartments",
+"Intramural Fields",
+"Allan Saxe Softball park",
+"Clay Gould Ballpark",
+"College hills park",
+"Doug Russel Park",
+"The green at college park",
+"UTA Tennis Center"];
     };
 
     $scope.clear = function () {
@@ -45,7 +129,6 @@ app.controller('appController', ['$scope', 'dataServ', '$http', '$window', funct
                 $scope.result.push(data);
 
                 var coordinates = JSON.parse(response.data[i].geom);
-                //console.log(coordinates.coordinates);
                 for (var j = 0; j < coordinates.coordinates.length; j++) {
 
                     var result = [];
@@ -56,23 +139,19 @@ app.controller('appController', ['$scope', 'dataServ', '$http', '$window', funct
                         data.push(r[0]);
                         result.push(data);
                     }
-                    console.log(result);
 
                     var bounds = result;
-
-                    //L.marker(result[0]).addTo(mymap).bindPopup(name).openPopup();
-
                     var data = { name: name, bounds: bounds };
 
                     $scope.detailedResult.push(data);
-
-                    // create an orange rectangle
                     L.polygon(bounds, { color: "#ff7800", weight: 1 }).addTo(mymap).bindPopup(name);
-
+                    if ($scope.marker) {
+                        L.marker($scope.detailedResult[i].bounds[0]).addTo(mymap).bindPopup($scope.detailedResult[i].name).openPopup();
+                    }
                     // zoom the map to the rectangle bounds
                     //mymap.fitBounds(bounds);
                 }
-            }
+            } 
         }, function errorCallback(response) {
             console.log(response);
         });
@@ -93,7 +172,6 @@ app.controller('appController', ['$scope', 'dataServ', '$http', '$window', funct
                 $scope.result.push(data);
 
                 var coordinates = JSON.parse(response.data[i].geom);
-                //console.log(coordinates.coordinates);
                 for (var j = 0; j < coordinates.coordinates.length; j++) {
 
                     var result = [];
@@ -104,19 +182,15 @@ app.controller('appController', ['$scope', 'dataServ', '$http', '$window', funct
                         data.push(r[0]);
                         result.push(data);
                     }
-                    console.log(result);
 
                     var bounds = result;
-
-                    //L.marker(result[0]).addTo(mymap).bindPopup(name).openPopup();
-
                     var data = { name: name, bounds: bounds };
 
                     $scope.detailedResult.push(data);
-
-                    // create an orange rectangle
                     L.polygon(bounds, { color: "#2980b9", weight: 1 }).addTo(mymap).bindPopup(name);
-
+                    if ($scope.marker) {
+                        L.marker($scope.detailedResult[i].bounds[0]).addTo(mymap).bindPopup($scope.detailedResult[i].name).openPopup();
+                    }
                     // zoom the map to the rectangle bounds
                     //mymap.fitBounds(bounds);
                 }
@@ -141,7 +215,6 @@ app.controller('appController', ['$scope', 'dataServ', '$http', '$window', funct
                 $scope.result.push(data);
 
                 var coordinates = JSON.parse(response.data[i].geom);
-                //console.log(coordinates.coordinates);
                 for (var j = 0; j < coordinates.coordinates.length; j++) {
 
                     var result = [];
@@ -152,19 +225,15 @@ app.controller('appController', ['$scope', 'dataServ', '$http', '$window', funct
                         data.push(r[0]);
                         result.push(data);
                     }
-                    console.log(result);
 
                     var bounds = result;
-
-                    //L.marker(result[0]).addTo(mymap).bindPopup(name).openPopup();
-
                     var data = { name: name, bounds: bounds };
 
                     $scope.detailedResult.push(data);
-
-                    // create an orange rectangle
                     L.polygon(bounds, { color: "#8e44ad", weight: 1 }).addTo(mymap).bindPopup(name);
-
+                    if ($scope.marker) {
+                        L.marker($scope.detailedResult[i].bounds[0]).addTo(mymap).bindPopup($scope.detailedResult[i].name).openPopup();
+                    }
                     // zoom the map to the rectangle bounds
                     //mymap.fitBounds(bounds);
                 }
@@ -189,7 +258,6 @@ app.controller('appController', ['$scope', 'dataServ', '$http', '$window', funct
                 $scope.result.push(data);
 
                 var coordinates = JSON.parse(response.data[i].geom);
-                //console.log(coordinates.coordinates);
                 for (var j = 0; j < coordinates.coordinates.length; j++) {
 
                     var result = [];
@@ -200,19 +268,15 @@ app.controller('appController', ['$scope', 'dataServ', '$http', '$window', funct
                         data.push(r[0]);
                         result.push(data);
                     }
-                    console.log(result);
 
                     var bounds = result;
-
-                    //L.marker(result[0]).addTo(mymap).bindPopup(name).openPopup();
-
                     var data = { name: name, bounds: bounds };
 
                     $scope.detailedResult.push(data);
-
-                    // create an orange rectangle
                     L.polygon(bounds, { color: "#27ae60", weight: 1 }).addTo(mymap).bindPopup(name);
-
+                    if ($scope.marker) {
+                        L.marker($scope.detailedResult[i].bounds[0]).addTo(mymap).bindPopup($scope.detailedResult[i].name).openPopup();
+                    }
                     // zoom the map to the rectangle bounds
                     //mymap.fitBounds(bounds);
                 }
@@ -222,34 +286,47 @@ app.controller('appController', ['$scope', 'dataServ', '$http', '$window', funct
         });
     }
 
-    $scope.findAllRoads = function () {
-        var data = [];
+    $scope.find = function () {
+        var data = { name: $scope.from };
 
         $http({
-            url: "/roads",
+            url: "/search",
             method: "get",
             params: { data: data }
         }).then(function successCallback(response) {
+            console.log(response);
             for (var i = 0; i < response.data.length; i++) {
                 var name = response.data[i].name;
+
+                var data = { name: name, type: "parks" };
+                $scope.result.push(data);
+
                 var coordinates = JSON.parse(response.data[i].geom);
                 for (var j = 0; j < coordinates.coordinates.length; j++) {
 
+                    var result = [];
+                    for (var k = 0; k < coordinates.coordinates[j][0].length; k++) {
+                        var r = coordinates.coordinates[j][0][k];
+                        var data = [];
+                        data.push(r[1]);
+                        data.push(r[0]);
+                        result.push(data);
+                    }
 
+                    var bounds = result;
+                    var data = { name: name, bounds: bounds };
+
+                    $scope.detailedResult.push(data);
+                    L.polygon(bounds, { color: "#27ae60", weight: 1 }).addTo(mymap).bindPopup(name);
+
+                    if ($scope.marker)
+                    {
+                        L.marker($scope.detailedResult[i].bounds[0]).addTo(mymap).bindPopup($scope.detailedResult[i].name).openPopup();
+                    }
+                    // zoom the map to the rectangle bounds
+                    //mymap.fitBounds(bounds);
                 }
             }
-            //var pointA = new L.LatLng(28.635308, 77.22496);
-            //var pointB = new L.LatLng(28.984461, 77.70641);
-            //var pointList = [pointA, pointB];
-
-            //var firstpolyline = new L.Polyline(pointList, {
-            //    color: 'red',
-            //    weight: 3,
-            //    opacity: 0.5,
-            //    smoothFactor: 1
-            //});
-            //firstpolyline.addTo(mymap);
-
         }, function errorCallback(response) {
             console.log(response);
         });
@@ -259,24 +336,25 @@ app.controller('appController', ['$scope', 'dataServ', '$http', '$window', funct
         $scope.result = [];
         $scope.detailedResult = [];
 
-        if ($scope.buildings) {
+        if ($scope.buildings && $scope.from == "") {
             $scope.findAllBuildings();
         }
-        if ($scope.onCampus) {
+        if ($scope.onCampus && $scope.from == "") {
             $scope.findAllOnCampus();
         }
-        if ($scope.offCampus) {
+        if ($scope.offCampus && $scope.from == "") {
             $scope.findAllOffCampus();
         }
-        if ($scope.parks) {
+        if ($scope.parks && $scope.from == "") {
             $scope.findAllParks();
         }
+
+        $scope.find();
     }
 
     $scope.popup = function (name) {
         for (var i = 0; i < $scope.detailedResult.length; i++) {
-            if ($scope.detailedResult[i].name == name)
-            {
+            if ($scope.detailedResult[i].name == name) {
                 L.marker($scope.detailedResult[i].bounds[0]).addTo(mymap).bindPopup($scope.detailedResult[i].name).openPopup();
             }
         }
--- server/routes.js
+++ server/routes.js
@@ -15,6 +15,33 @@ module.exports = function (app) {
 		res.json(data);
 	});
 
+	app.get('/search', function (req, res) {
+
+	    var q = JSON.parse(req.query.data);  
+
+	    var data = [];
+	    var found = false;
+
+	    var buildingSearch = "SELECT name,st_asgeojson(geom) as geom FROM buildings WHERE name = '" + q.name + "'";
+	    var onCampus = "SELECT name,st_asgeojson(geom) as geom FROM "+'"onCampus"'+" WHERE name ='" + q.name + "'";
+	    var offCampus = "SELECT name,st_asgeojson(geom) as geom FROM "+'"offCampus"'+"WHERE name ='" + q.name + "'";
+	    var park = "SELECT name,st_asgeojson(geom) as geom FROM " + '"parks"' + " WHERE name ='" + q.name + "'";
+
+	    var search = buildingSearch + " UNION " + onCampus + " UNION " + offCampus + " UNION " + park;
+
+	    console.log(search);
+
+	    var query = client.query(search);
+	    
+	    query.on('row', function (row) {
+	        data.push(row);
+	    });
+
+	    query.on('end', function () {
+	        res.json(data);
+	    });
+	});
+
 	app.get('/buildings', function (req, res) {
 	    var data = [];
 	    var query = client.query("SELECT name,st_asgeojson(geom) as geom FROM buildings");
@@ -29,7 +56,7 @@ module.exports = function (app) {
 
 	app.get('/onCampus', function (req, res) {
 	    var data = [];
-	    var query = client.query('SELECT name,st_asgeojson(geom) as geom FROM "on-campus-apartments"');
+	    var query = client.query('SELECT name,st_asgeojson(geom) as geom FROM "onCampus"');
 	    query.on('row', function (row) {
 	        data.push(row);
 	    });
@@ -41,7 +68,7 @@ module.exports = function (app) {
 
 	app.get('/offCampus', function (req, res) {
 	    var data = [];
-	    var query = client.query('SELECT name,st_asgeojson(geom) as geom FROM "off-campus-apartments"');
+	    var query = client.query('SELECT name,st_asgeojson(geom) as geom FROM "offCampus"');
 	    query.on('row', function (row) {
 	        data.push(row);
 	    });
@@ -65,7 +92,7 @@ module.exports = function (app) {
 
 	app.get('/parks', function (req, res) {
 	    var data = [];
-	    var query = client.query('SELECT name,st_asgeojson(geom) as geom FROM "parks-and-playgrounds"');
+	    var query = client.query('SELECT name,st_asgeojson(geom) as geom FROM "parks"');
 	    query.on('row', function (row) {
 	        data.push(row);
 	    });
